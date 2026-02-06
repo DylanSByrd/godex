1fb9d19c911c62b53ff0ff90bc9e57ad07a68d4a
DylanSByrd <mail@dylansbyrd.com>
2026-02-06 10:00:46 -0500

    adding help and exit commands

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		tokens := cleanInput(input)
-		if len(tokens) != 0 {
-			fmt.Printf("Your command was: %v", tokens[0])
-		}
-		fmt.Println("")
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"strings"
+	"fmt"
+	"os"
+	"bufio"
 )
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	cmds := getCommands()
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		input := scanner.Text()
+		tokens := cleanInput(input)
+
+		if len(tokens) != 0 {
+			cmd, ok := cmds[tokens[0]]
+			if ok {
+				err := cmd.callback()
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				fmt.Println("Uknown command")
+			}
+		}
+	}
+}
